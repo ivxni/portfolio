420c5f5c2fd42ae1c8dda794407b4950b172e967
ivxni <angelmxycry@outlook.com>
2025-03-19 14:02:59 +0100

    Setup

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './utils/fontawesome';
+import './styles/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
