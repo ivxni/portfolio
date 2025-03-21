169f6048551adeba6b7ab3748cd3593c6b4f89d1
ivxni <angelmxycry@outlook.com>
2025-03-21 22:40:43 +0100

    fix

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import projects from '../data/projects';
 import './ProjectsPage.scss';
 
